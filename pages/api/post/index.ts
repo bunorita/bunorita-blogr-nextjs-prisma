bb39da5168de1d91ec8075c0b05069cef06d8c9b
Buntaro Orita <buntaro.orita@gmail.com>
2023-04-27 11:14:33 +0900

    Adde new post

---
+++ pages/api/post/index.ts
@@ -0,0 +1,25 @@
+// pages/api/post/index.ts
+
+import { getSession } from 'next-auth/react';
+import prisma from '../../../lib/prisma';
+import { getServerSession } from 'next-auth';
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  console.log('req: ', req);
+  const session = await getSession({ req });
+  console.log('session: ', session);
+
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
