8afbf1cf9736a47b21a5157a4afae774234bbe84
Buntaro Orita <buntaro.orita@gmail.com>
2023-04-27 16:44:24 +0900

    Delete post

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -26,6 +26,11 @@ async function publishPost(id: string): Promise<void> {
   await Router.push('/');
 }
 
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, { method: 'DELETE' })
+  Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
   const { data: session, status } = useSession();
   if (status === 'loading') {
@@ -47,6 +52,10 @@ const Post: React.FC<PostProps> = (props) => {
         {!props.published && userHasValidSession && postBelongsToUser && (
           <button onClick={() => publishPost(props.id)}>Publish</button>
         )}
+        {
+          userHasValidSession && postBelongsToUser && (
+            <button onClick={() => deletePost(props.id)}>Delete</button>
+          )}
       </div>
       <style jsx>{`
         .page {
